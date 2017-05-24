56fd668b4bc09611590aaae4a52ad5f1da799623
Mairin Bailey <mairin.bailey@gmail.com>
2017-05-24 14:16:29 -0600

    create recipe class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is a test', 'https://www.sesdweb.net/cms/lib/PA01000019/Centricity/Domain/267/recipe.gif')
+  ];
 
   constructor() { }
 
